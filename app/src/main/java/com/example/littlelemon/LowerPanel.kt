94f00c219041cf04f3f019a622babce96d82ac65
Jhoan Ordenes Galleguillos <johan.e.galleguillos@accenture.com>
2023-02-07 19:38:26 -0300

    1. setup key elements

--- app/src/main/java/com/example/littlelemon/LowerPanel.kt
+++ app/src/main/java/com/example/littlelemon/LowerPanel.kt
@@ -1,7 +1,9 @@
 package com.example.littlelemon
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -9,6 +11,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -48,6 +51,17 @@ fun MenuDish(navController: NavHostController? = null, dish: Dish) {
         navController?.navigate(DishDetails.route + "/${dish.id}")
     }) {
         //TODO: Insert code here
+        Row {
+            Column {
+                Text(text = "Text 1")
+                Text(text = "Text 2")
+                Text(text = "Text 3")
+            }
+            Image(
+                painter = painterResource(id = R.drawable.bruschetta),
+                contentDescription = ""
+            )
+        }
     }
     Divider(
         modifier = Modifier.padding(start = 8.dp, end = 8.dp),
