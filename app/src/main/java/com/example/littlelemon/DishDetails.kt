4276fafb1b806bc2c47af6914ffd10874bb970ba
Jhoan Ordenes Galleguillos <johan.e.galleguillos@accenture.com>
2023-02-07 20:42:00 -0300

    3. add the dish details

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -27,11 +28,11 @@ fun DishDetails(id: Int) {
             contentScale = ContentScale.FillWidth
         )
         Column {
-            Text(text = "Text 1")
-            Text(text = "Text 2")
+            Text(text = dish.name)
+            Text(text = dish.description)
             Counter()
             Button(onClick = { /*TODO*/ }) {
-                Text(text = "Text 3")
+                Text(text = stringResource(id = R.string.add_for) + " $${dish.price}")
             }
         }
     }
