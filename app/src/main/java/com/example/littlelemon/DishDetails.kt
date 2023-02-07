6b8a3e4d3e6a7f76c83be440665a98952ce92936
Jhoan Ordenes Galleguillos <johan.e.galleguillos@accenture.com>
2023-02-07 20:38:27 -0300

    1. setup the ui elements

--- app/src/main/java/com/example/littlelemon/DishDetails.kt
+++ app/src/main/java/com/example/littlelemon/DishDetails.kt
@@ -1,12 +1,15 @@
 package com.example.littlelemon
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -15,6 +18,16 @@ fun DishDetails(id: Int) {
     val dish = requireNotNull(DishRepository.getDish(id))
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
         //TODO: Insert code here
+        TopAppBar()
+        Image(painter = painterResource(id = R.drawable.bruschetta), contentDescription = "")
+        Column {
+            Text(text = "Text 1")
+            Text(text = "Text 2")
+            Counter()
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Text 3")
+            }
+        }
     }
 }
 
